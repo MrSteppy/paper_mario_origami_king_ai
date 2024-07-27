08eb2a89c649ed9f861868272043696ee9fb498f
Steppy <jonlaurem@gmail.com>
2024-07-27 19:10:29 +0200

    Move shader pre-processor to own crate

--- gui/build.rs
+++ gui/build.rs
@@ -1,20 +1,12 @@
-use std::collections::HashSet;
-use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::Path;
 use std::process::Command;
-use std::str::FromStr;
-use std::{fs, io};
 
-use lazy_static::lazy_static;
-use regex::Regex;
 use wgsl_to_wgpu::{create_shader_module, MatrixVectorTypes, WriteOptions};
 
+use shader_pre_processor::{pre_process_shader, ProcessContext};
+
 const INCLUDE_HOOK_POINT: &str = "INCLUDE_HOOK_POINT";
-///The prefix of every pre-processor statement
-const STMT_PREFIX: &str = "#";
-const NO_STANDALONE: &str = "no-standalone";
-const INCLUDE: &str = "include";
-const INCLUDE_ONLY_ONCE: &str = "once";
-const GEN_REPR: &str = "genRepr";
 
 fn main() {
   println!("cargo::rerun-if-changed=resources/shader/**");
@@ -37,36 +29,33 @@ fn main() {
     if let Some(shader_name) = file_name.strip_suffix(".wgsl").map(|s| s.to_string()) {
       println!("Processing shader {}...", shader_name);
 
-      let pre_process_info = pre_process_shader(
-        path.to_string_lossy().as_ref(),
-        None,
+      if let Some(pre_process_info) = pre_process_shader(
+        &path,
         ProcessContext::Standalone,
       )
-      .expect("failed to pre-process shader");
-      if pre_process_info.no_standalone {
+      .expect("failed to pre-process shader") {
+        let shader_module_source = create_shader_module(
+          &pre_process_info.source_code,
+          INCLUDE_HOOK_POINT,
+          WriteOptions {
+            derive_bytemuck_vertex: true,
+            derive_encase_host_shareable: true,
+            matrix_vector_types: MatrixVectorTypes::Glam,
+            ..Default::default()
+          },
+        )
+          .expect("can't convert shader to rust");
+        let shader_module_source = shader_module_source.replace(
+          &format!("include_str!(\"{}\")", INCLUDE_HOOK_POINT),
+          &format!("r#\"\n{}\"#", pre_process_info.source_code),
+        );
+
+        shader_rs_source += &format!("pub mod {} {{\n{}\n}}\n", shader_name, shader_module_source);
+        
+        println!("Ok!");
+      } else {
         println!("No standalone - ignoring");
-        continue;
       }
-
-      let shader_module_source = create_shader_module(
-        &pre_process_info.source_code,
-        INCLUDE_HOOK_POINT,
-        WriteOptions {
-          derive_bytemuck_vertex: true,
-          derive_encase_host_shareable: true,
-          matrix_vector_types: MatrixVectorTypes::Glam,
-          ..Default::default()
-        },
-      )
-      .expect("can't convert shader to rust");
-      let shader_module_source = shader_module_source.replace(
-        &format!("include_str!(\"{}\")", INCLUDE_HOOK_POINT),
-        &format!("r#\"\n{}\"#", pre_process_info.source_code),
-      );
-
-      shader_rs_source += &format!("pub mod {} {{\n{}\n}}\n", shader_name, shader_module_source);
-
-      println!("Ok!");
     }
   }
 
@@ -78,152 +67,3 @@ fn main() {
   }
 }
 
-fn pre_process_shader<'a, I>(
-  shader_file: &str,
-  already_included_files: I,
-  context: ProcessContext,
-) -> Result<PreProcessingInfo, PreProcessingError>
-where
-  I: Into<Option<&'a HashSet<String>>>,
-{
-  let shader_source = fs::read_to_string(shader_file).map_err(|e| PreProcessingError::IO {
-    error: e,
-    file: Path::new(shader_file).to_path_buf(),
-  })?;
-
-  let mut info = PreProcessingInfo {
-    included_files: already_included_files.into().cloned().unwrap_or_default(),
-    ..Default::default()
-  };
-  for (line_index, line) in shader_source.lines().enumerate() {
-    let line_nr = line_index + 1;
-    if get_statement(line, NO_STANDALONE).is_some() {
-      info.no_standalone = true;
-      if context == ProcessContext::Standalone {
-        return Ok(info);
-      }
-      continue;
-    }
-
-    if get_statement(line, INCLUDE_ONLY_ONCE).is_some() {
-      info.include_only_once = true;
-      if info.included_files.contains(shader_file) {
-        return Ok(info);
-      }
-      continue;
-    }
-
-    if let Some(include_statement) = get_statement(line, INCLUDE) {
-      let include = &include_statement.arg_str;
-      let include_file = Path::new(shader_file)
-        .parent()
-        .unwrap()
-        .join(include)
-        .to_string_lossy()
-        .to_string();
-      let mut include_file_info =
-        pre_process_shader(&include_file, &info.included_files, ProcessContext::Include)?;
-      if include_file_info.include_only_once && info.included_files.contains(include) {
-        continue;
-      }
-      info.source_code += &include_file_info.source_code;
-      info.included_files.insert(include.to_string());
-      info
-        .struct_definitions
-        .append(&mut include_file_info.struct_definitions);
-      info
-        .generated_representations
-        .append(&mut include_file_info.generated_representations);
-      for included_file in include_file_info.included_files {
-        info.included_files.insert(included_file);
-      }
-      continue;
-    }
-
-    if let Some(gen_repr_statement) = get_statement(line, GEN_REPR) {
-      //TODO generate struct representation
-      continue;
-    }
-
-    //we assume a somewhat formatted shader file
-    if let Some(struct_name) = line.strip_prefix("struct ") {
-      //TODO parse struct definition
-    }
-
-    info.source_code += &format!("{}\n", line);
-  }
-
-  Ok(info)
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Default)]
-enum ProcessContext {
-  #[default]
-  Standalone,
-  Include,
-}
-
-#[derive(Debug)]
-struct StructDefinition {
-  name: String,
-  members: Vec<StructMember>,
-}
-
-#[derive(Debug)]
-struct StructMember {
-  annotations: Vec<String>,
-  name: String,
-  r#type: String,
-}
-
-impl FromStr for StructDefinition {
-  type Err = String;
-
-  fn from_str(s: &str) -> Result<Self, Self::Err> {
-    lazy_static! {
-      static ref STRUCT_REGEX: Regex =
-        Regex::new(r"^struct (\S+) \{([\s\S]*?)};?").expect("invalid regex");
-      static ref MEMBER_REGEX: Regex =
-        Regex::new(r"\s*((@\S+\s*)*)(\S+): (\S+),\s*").expect("invalid regex");
-    }
-
-    todo!()
-  }
-}
-
-#[derive(Debug)]
-struct GeneratedRepresentation {
-  name: String,
-  ///number of bytes divided by four
-  size: usize,
-}
-
-fn get_statement(line: &str, statement: &str) -> Option<PreProcessorStatement> {
-  let prefix = format!("{}{}", STMT_PREFIX, statement);
-  line
-    .strip_prefix(&prefix)
-    .map(|arg_str| PreProcessorStatement {
-      arg_str: arg_str.trim().to_string(),
-    })
-}
-
-#[derive(Debug)]
-struct PreProcessorStatement {
-  arg_str: String,
-}
-
-#[derive(Debug, Default)]
-struct PreProcessingInfo {
-  pub included_files: HashSet<String>,
-  pub source_code: String,
-  pub struct_definitions: Vec<StructDefinition>,
-  pub generated_representations: Vec<GeneratedRepresentation>,
-  pub no_standalone: bool,
-  pub include_only_once: bool,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-enum PreProcessingError {
-  IO { error: io::Error, file: PathBuf },
-}
---
+++ shader_pre_processor/src/lib.rs
@@ -0,0 +1,288 @@
+use std::{fs, io};
+use std::collections::HashSet;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+
+use enum_assoc::Assoc;
+use once_cell_regex::regex;
+
+///The prefix of every pre-processor statement
+pub const STMT_PREFIX: &str = "#";
+
+///All supported pre-processor statements
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Assoc)]
+#[func(pub fn as_str(&self) -> &str)]
+pub enum Statement {
+  ///Mark a file to be ignored by normal shader processing
+  #[assoc(as_str = "no-standalone")]
+  NoStandalone,
+  ///<file> - Include another file -> Copy-paste it into the code
+  #[assoc(as_str = "include")]
+  Include,
+  ///If this file gets included, do so only once
+  #[assoc(as_str = "once")]
+  IncludeOnlyOnce,
+  ///<struct> [repr-name] - Generate a representation of the struct which can be translated by wgsl_to_wgpu.
+  #[assoc(as_str = "genRepr")]
+  GenRepr,
+}
+
+impl Statement {
+  pub fn match_info(&self, line: &str) -> Option<StatementInfo> {
+    line
+      .strip_prefix(&format!("{}{}", STMT_PREFIX, self.as_str()))
+      .map(|arg_str| StatementInfo {
+        arg_str: arg_str.trim().to_string(),
+      })
+  }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct StatementInfo {
+  arg_str: String,
+}
+
+///Pre-processes a shader file. Will return None when pre-processing is cancelled early because file
+/// has already been included or should not be processed as standalone.
+pub fn pre_process_shader<P>(
+  shader_file: P,
+  context: ProcessContext,
+) -> Result<Option<PreProcessedShaderInfo>, PreProcessingError>
+where
+  P: AsRef<Path>,
+{
+  let shader_file = shader_file.as_ref();
+
+  let shader_source = fs::read_to_string(shader_file).map_err(|e| PreProcessingError::IO {
+    error: e,
+    file: shader_file.to_path_buf(),
+  })?;
+
+  let mut info = PreProcessedShaderInfo {
+    included_files: Default::default(),
+    ..Default::default()
+  };
+  for (line_index, line) in shader_source.lines().enumerate() {
+    if Statement::NoStandalone.match_info(line).is_some() {
+      info.no_standalone = true;
+      if let ProcessContext::Standalone = &context {
+        return Ok(None);
+      }
+      continue;
+    }
+
+    if Statement::IncludeOnlyOnce.match_info(line).is_some() {
+      info.include_only_once = true;
+      if let ProcessContext::Include {
+        previous_includes, ..
+      } = &context
+      {
+        let shader_file_str = shader_file
+          .to_str()
+          .expect("shader file path not valid utf-8, should not have been able to be included")
+          .to_string();
+        if previous_includes.contains(&shader_file_str) {
+          return Ok(None);
+        }
+      }
+      continue;
+    }
+
+    if let Some(stmt_info) = Statement::Include.match_info(line) {
+      let to_include = &stmt_info.arg_str;
+      let include_file = shader_file
+        .parent()
+        .expect("can't access shader directory")
+        .join(to_include);
+
+      if let Some(include_file_info) = pre_process_shader(include_file, context.join(&info))? {
+        info.absorb(include_file_info);
+      }
+
+      continue;
+    }
+
+    if let Some(stmt_info) = Statement::GenRepr.match_info(line) {
+      //TODO generate struct representation
+      continue;
+    }
+
+    if let Ok(struct_definition) = shader_source
+      .lines()
+      .skip(line_index)
+      .collect::<Vec<_>>()
+      .join("\n")
+      .parse::<StructDefinition>()
+    {
+      info.struct_definitions.push(struct_definition);
+    }
+
+    info.source_code += &format!("{}\n", line);
+  }
+
+  Ok(Some(info))
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub enum ProcessContext<'a> {
+  #[default]
+  Standalone,
+  Include {
+    previous_includes: HashSet<&'a String>,
+    previous_struct_definitions: Vec<&'a StructDefinition>,
+  },
+}
+
+impl<'a> ProcessContext<'a> {
+  pub fn join(&'a self, info: &'a PreProcessedShaderInfo) -> ProcessContext<'a> {
+    let mut includes = HashSet::new();
+    let mut struct_definitions = vec![];
+
+    for include in &info.included_files {
+      includes.insert(include);
+    }
+    for struct_definition in &info.struct_definitions {
+      struct_definitions.push(struct_definition);
+    }
+
+    if let ProcessContext::Include {
+      previous_includes,
+      previous_struct_definitions,
+    } = self
+    {
+      for include in previous_includes {
+        includes.insert(include);
+      }
+      for struct_definition in previous_struct_definitions {
+        struct_definitions.push(struct_definition);
+      }
+    }
+
+    Self::Include {
+      previous_includes: includes,
+      previous_struct_definitions: struct_definitions,
+    }
+  }
+}
+
+#[derive(Debug, Default)]
+pub struct PreProcessedShaderInfo {
+  pub included_files: HashSet<String>,
+  pub source_code: String,
+  pub struct_definitions: Vec<StructDefinition>,
+  pub generated_representations: Vec<GeneratedRepresentation>,
+  pub no_standalone: bool,
+  pub include_only_once: bool,
+}
+
+impl PreProcessedShaderInfo {
+  pub fn absorb(&mut self, info: PreProcessedShaderInfo) {
+    let PreProcessedShaderInfo {
+      included_files,
+      source_code,
+      mut struct_definitions,
+      mut generated_representations,
+      ..
+    } = info;
+    for include in included_files {
+      self.included_files.insert(include);
+    }
+    self.source_code += &source_code;
+    self.struct_definitions.append(&mut struct_definitions);
+    self
+      .generated_representations
+      .append(&mut generated_representations);
+  }
+}
+
+#[derive(Debug)]
+pub struct GeneratedRepresentation {
+  pub name: String,
+  ///number of bytes divided by four
+  pub size: usize,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum PreProcessingError {
+  IO { error: io::Error, file: PathBuf },
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct StructDefinition {
+  pub name: String,
+  pub members: Vec<StructMember>,
+}
+
+impl FromStr for StructDefinition {
+  type Err = String;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let struct_regex = regex!(r"^struct (?<name>\S+)\s*\{(?<content>[\s\S]*?)};?");
+    let member_regex = regex!(r"\s*(?<annotations>(@\S+\s*)*)(?<name>\S+): (?<type>\S+),\s*");
+
+    let captures = struct_regex
+      .captures(s)
+      .filter(|caps| caps.get(0).unwrap().start() == 0)
+      .ok_or("provided string doesn't start with a valid struct definition")?;
+    let struct_name = captures.name("name").unwrap().as_str().to_string();
+    let struct_content = captures.name("content").unwrap().as_str();
+
+    let mut members = vec![];
+    for captures in member_regex.captures_iter(struct_content) {
+      let member_name = captures.name("name").unwrap().as_str().to_string();
+      let annotations: Vec<String> = captures
+        .name("annotations")
+        .unwrap()
+        .as_str()
+        .split_whitespace()
+        .map(|annotation| annotation.to_string())
+        .collect();
+      let member_type = captures.name("type").unwrap().as_str().to_string();
+      members.push(StructMember {
+        annotations,
+        name: member_name,
+        r#type: member_type,
+      });
+    }
+
+    Ok(StructDefinition {
+      name: struct_name,
+      members,
+    })
+  }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct StructMember {
+  pub annotations: Vec<String>,
+  pub name: String,
+  pub r#type: String,
+}
+
+#[cfg(test)]
+mod test {
+  use crate::{pre_process_shader, ProcessContext, StructDefinition};
+
+  #[test]
+  fn test_parse_struct_definition() {
+    let definition: StructDefinition = r"struct Pixel {
+      @location(0) x: u32,
+      @location(1) y: u32,
+    }"
+    .parse()
+    .expect("failed to parse struct definition");
+    assert_eq!("Pixel", definition.name);
+    assert_eq!("x", definition.members[0].name);
+    assert_eq!("u32", definition.members[0].r#type);
+  }
+
+  #[test]
+  fn test_pre_processing() {
+    pre_process_shader(
+      env!("CARGO_MANIFEST_DIR").to_string() + "/../gui/resources/shader/texture_shader.wgsl",
+      ProcessContext::Standalone,
+    )
+    .expect("failed to pre-process valid shader code");
+  }
+}
