27f15b92aa11fa7a3473617671528482a468d278
Steppy <jonlaurem@gmail.com>
2024-07-28 21:48:51 +0200

    Implement memory_layout.rs

--- shader_pre_processor/src/memory_layout.rs
+++ shader_pre_processor/src/memory_layout.rs
@@ -1,24 +1,117 @@
+use std::collections::{HashMap, HashSet};
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use std::iter::once;
 
-use enum_assoc::Assoc;
+use crate::struct_definition::StructDefinition;
 
-use crate::StructDefinition;
+pub type StructDefinitionCache = HashMap<String, (StructDefinition, Option<MemoryLayout>)>;
 
-//TODO better use a type resolver instead of the direct struct definitions
-pub fn create_memory_layout(struct_definition: &StructDefinition, other_struct_definitions: &Vec<&StructDefinition>) -> MemoryLayout {
-  todo!()
+#[derive(Debug)]
+pub struct TypeResolver<'a> {
+  pub primitive_types: &'a HashSet<String>,
+  pub struct_definition_cache: &'a mut StructDefinitionCache,
+}
+
+pub fn create_memory_layout(
+  target_type_name: &str,
+  type_resolver: &mut TypeResolver,
+) -> Result<MemoryLayout, MemoryLayoutCreationError> {
+  if type_resolver.primitive_types.contains(target_type_name) {
+    return Ok(MemoryLayout::Primitive(PrimitiveLayout {
+      primitive_type: target_type_name.to_string(),
+    }));
+  }
+
+  if let Some((struct_definition, memory_layout)) =
+    type_resolver.struct_definition_cache.get(target_type_name)
+  {
+    if let Some(memory_layout) = memory_layout {
+      return Ok(memory_layout.clone());
+    }
+
+    let struct_definition = struct_definition.clone();
+
+    //attempt to generate new layout
+    let mut layout = CompositeLayout::new(&struct_definition.name);
+    for member in &struct_definition.members {
+      layout.add(
+        create_memory_layout(&member.r#type, type_resolver).map_err(|e| {
+          MemoryLayoutCreationError {
+            target_type_name: target_type_name.to_string(),
+            reason: MemoryLayoutCreationErrorReason::MemberLayout {
+              member_name: member.name.clone(),
+              error: Box::new(e),
+            },
+          }
+        })?,
+      )
+    }
+
+    let layout: MemoryLayout = layout.into();
+
+    type_resolver
+      .struct_definition_cache
+      .get_mut(target_type_name)
+      .unwrap()
+      .1 = Some(layout.clone());
+
+    return Ok(layout);
+  }
+
+  Err(MemoryLayoutCreationError {
+    target_type_name: target_type_name.to_string(),
+    reason: MemoryLayoutCreationErrorReason::UnknownType,
+  })
+}
+
+#[derive(Debug)]
+pub struct MemoryLayoutCreationError {
+  pub target_type_name: String,
+  pub reason: MemoryLayoutCreationErrorReason,
+}
+
+impl Display for MemoryLayoutCreationError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "Can't create memory layout for type {}: {}",
+      self.target_type_name, self.reason
+    )
+  }
+}
+
+impl Error for MemoryLayoutCreationError {}
+
+#[derive(Debug)]
+pub enum MemoryLayoutCreationErrorReason {
+  UnknownType,
+  MemberLayout {
+    member_name: String,
+    error: Box<MemoryLayoutCreationError>,
+  },
+}
+
+impl Display for MemoryLayoutCreationErrorReason {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    match self {
+      MemoryLayoutCreationErrorReason::UnknownType => write!(
+        f,
+        "Unknown type: Not a primitive or previously defined struct"
+      ),
+      MemoryLayoutCreationErrorReason::MemberLayout { member_name, error } => write!(
+        f,
+        "Can't create memory layout for member '{}': {}",
+        member_name, error
+      ),
+    }
+  }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum MemoryLayout {
   Composed(CompositeLayout),
   Primitive(PrimitiveLayout),
-}
-
-impl Default for MemoryLayout {
-  fn default() -> Self {
-    Self::Primitive(Default::default())
-  }
 }
 
 impl From<CompositeLayout> for MemoryLayout {
@@ -78,9 +171,9 @@ impl MemoryLayoutCommon for CompositeLayout {
   }
 }
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Default)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct PrimitiveLayout {
-  pub primitive_type: PrimitiveType,
+  pub primitive_type: String,
 }
 
 impl MemoryLayoutCommon for PrimitiveLayout {
@@ -93,15 +186,73 @@ pub trait MemoryLayoutCommon {
   fn iter(&self) -> impl Iterator<Item = &PrimitiveLayout>;
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Default, Assoc)]
-#[func(pub const fn name(&self) -> &'static str)]
-#[func(pub fn from_name(name: &str) -> Option<Self>)]
-pub enum PrimitiveType {
-  #[default]
-  #[assoc(name = "f32")]
-  #[assoc(from_name = "f32")]
-  F32,
-  #[assoc(name = "u32")]
-  #[assoc(from_name = "u32")]
-  U32,
+#[cfg(test)]
+mod test {
+  use std::collections::{HashMap, HashSet};
+
+  use crate::memory_layout::{
+    CompositeLayout, create_memory_layout, MemoryLayout, PrimitiveLayout, TypeResolver,
+  };
+  use crate::struct_definition::{StructDefinition, StructMember};
+
+  #[test]
+  fn test_create_memory_layout() {
+    let mut struct_definition_cache: HashMap<String, (StructDefinition, Option<MemoryLayout>)> =
+      HashMap::new();
+    struct_definition_cache.insert(
+      "Pixel".to_string(),
+      (
+        StructDefinition {
+          name: "Pixel".to_string(),
+          members: vec![
+            StructMember {
+              annotations: vec![],
+              name: "x".to_string(),
+              r#type: "f32".to_string(),
+            },
+            StructMember {
+              annotations: vec![],
+              name: "y".to_string(),
+              r#type: "f32".to_string(),
+            },
+          ],
+        },
+        None,
+      ),
+    );
+    let mut primitive_types = HashSet::new();
+    primitive_types.insert("f32".to_string());
+    primitive_types.insert("u32".to_string());
+    let mut type_resolver = TypeResolver {
+      primitive_types: &primitive_types,
+      struct_definition_cache: &mut struct_definition_cache,
+    };
+
+    let layout =
+      create_memory_layout("Pixel", &mut type_resolver).expect("failed to create memory layout");
+    assert_eq!(
+      MemoryLayout::Composed(CompositeLayout {
+        type_name: "Pixel".to_string(),
+        components: vec![
+          MemoryLayout::Primitive(PrimitiveLayout {
+            primitive_type: "f32".to_string(),
+          }),
+          MemoryLayout::Primitive(PrimitiveLayout {
+            primitive_type: "f32".to_string(),
+          }),
+        ],
+      }),
+      layout
+    );
+
+    assert_eq!(
+      *struct_definition_cache
+        .get("Pixel")
+        .unwrap()
+        .1
+        .as_ref()
+        .expect("cached memory layout missing"),
+      layout
+    );
+  }
 }
---
+++ shader_pre_processor/src/struct_definition.rs
@@ -0,0 +1,83 @@
+use std::str::FromStr;
+
+use once_cell_regex::regex;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct StructDefinition {
+  pub name: String,
+  pub members: Vec<StructMember>,
+}
+
+impl FromStr for StructDefinition {
+  type Err = String;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let struct_regex = regex!(r"^struct (?<name>\S+)\s*\{(?<content>[\s\S]*?)};?");
+    let member_regex = regex!(r"\s*(?<annotations>(@\S+\s*)*)(?<name>\S+): (?<type>\S+),\s*");
+
+    let captures = struct_regex
+      .captures(s)
+      .filter(|caps| caps.get(0).unwrap().start() == 0)
+      .ok_or("provided string doesn't start with a valid struct definition")?;
+    let struct_name = captures.name("name").unwrap().as_str().to_string();
+    let struct_content = captures.name("content").unwrap().as_str();
+
+    let mut members = vec![];
+    for captures in member_regex.captures_iter(struct_content) {
+      let member_name = captures.name("name").unwrap().as_str().to_string();
+      let annotations: Vec<String> = captures
+        .name("annotations")
+        .unwrap()
+        .as_str()
+        .split_whitespace()
+        .map(|annotation| annotation.to_string())
+        .collect();
+      let member_type = captures.name("type").unwrap().as_str().to_string();
+      members.push(StructMember {
+        annotations,
+        name: member_name,
+        r#type: member_type,
+      });
+    }
+
+    Ok(StructDefinition {
+      name: struct_name,
+      members,
+    })
+  }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct StructMember {
+  pub annotations: Vec<String>,
+  pub name: String,
+  pub r#type: String,
+}
+
+#[cfg(test)]
+mod test {
+  use crate::{pre_process_shader, ProcessContext};
+  use crate::struct_definition::StructDefinition;
+
+  #[test]
+  fn test_parse_struct_definition() {
+    let definition: StructDefinition = r"struct Pixel {
+      @location(0) x: u32,
+      @location(1) y: u32,
+    }"
+    .parse()
+    .expect("failed to parse struct definition");
+    assert_eq!("Pixel", definition.name);
+    assert_eq!("x", definition.members[0].name);
+    assert_eq!("u32", definition.members[0].r#type);
+  }
+
+  #[test]
+  fn test_pre_processing() {
+    pre_process_shader(
+      env!("CARGO_MANIFEST_DIR").to_string() + "/../gui/resources/shader/texture_shader.wgsl",
+      ProcessContext::Standalone,
+    )
+    .expect("failed to pre-process valid shader code");
+  }
+}
