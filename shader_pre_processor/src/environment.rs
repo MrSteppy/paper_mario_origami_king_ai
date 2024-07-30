ec7b768dafd65ec3997d8542300fbddaa7fcf1f2
Steppy <jonlaurem@gmail.com>
2024-07-30 18:53:46 +0200

    Added MemoryLayout to environment.rs

--- shader_pre_processor/src/environment.rs
+++ shader_pre_processor/src/environment.rs
@@ -13,27 +13,33 @@ pub struct PrimitiveType {
   pub size_in_alignments: usize,
   ///the power of two to how many bytes this is aligned
   pub alignment_power: usize,
+  ///The fully qualified path to a rust equivalent type
+  pub rust_equivalent: String,
 }
 
 impl PrimitiveType {
-  pub fn new<S>(name: S, size: usize) -> Self
+  pub fn new<S, E>(name: S, size: usize, rust_equivalent: E) -> Self
   where
     S: ToString,
+    E: ToString,
   {
     Self {
       name: name.to_string(),
       size_in_alignments: size,
       alignment_power: 0,
+      rust_equivalent: rust_equivalent.to_string(),
     }
   }
 
-  pub fn new_aligned<S>(
+  pub fn new_aligned<S, E>(
     name: S,
     size: usize,
     alignment: usize,
+    rust_equivalent: E,
   ) -> Result<Self, PrimitiveTypeCreationError>
   where
     S: ToString,
+    E: ToString,
   {
     if !alignment.is_power_of_two() {
       return Err(PrimitiveTypeCreationError::InvalidAlignment);
@@ -46,6 +52,7 @@ impl PrimitiveType {
       name: name.to_string(),
       size_in_alignments: size / alignment,
       alignment_power: alignment.ilog2() as usize,
+      rust_equivalent: rust_equivalent.to_string(),
     })
   }
 
@@ -64,6 +71,22 @@ impl PartialOrd for PrimitiveType {
 impl Ord for PrimitiveType {
   fn cmp(&self, other: &Self) -> Ordering {
     self.size().cmp(&other.size())
+  }
+}
+
+impl Display for PrimitiveType {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "{}[{}{}byte]",
+      self.name,
+      self.size_in_alignments,
+      if self.alignment_power > 0 {
+        format!("x{}", self.alignment_power)
+      } else {
+        "".to_string()
+      }
+    )
   }
 }
 
@@ -106,11 +129,20 @@ impl CompositeType {
     }
   }
 
-  pub fn add<M>(&mut self, member: M)
+  pub fn with_member<M>(mut self, member: M) -> Self
   where
     M: Into<Member>,
   {
     self.members.push(member.into());
+    self
+  }
+
+  pub fn add<M>(&mut self, member: M) -> &mut Self
+  where
+    M: Into<Member>,
+  {
+    self.members.push(member.into());
+    self
   }
 
   pub fn primitive_iter(&self) -> impl Iterator<Item = &PrimitiveType> {
@@ -118,6 +150,22 @@ impl CompositeType {
       .members
       .iter()
       .flat_map(|member| member.r#type.primitive_iter())
+  }
+}
+
+impl Display for CompositeType {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "{} ({})",
+      self.name,
+      self
+        .members
+        .iter()
+        .map(|member| member.to_string())
+        .collect::<Vec<_>>()
+        .join(", ")
+    )
   }
 }
 
@@ -156,23 +204,57 @@ impl Member {
   }
 }
 
+impl Display for Member {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "{}{}: {}",
+      self
+        .annotation_values
+        .iter()
+        .map(|value| format!("@{value} "))
+        .collect::<Vec<_>>()
+        .join(""),
+      self.name,
+      self.r#type
+    )
+  }
+}
+
+///Describes a field in a [`MemoryLayout`]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct PrimitiveMember {
+  name: String,
+  r#type: PrimitiveType,
+}
+
+impl PrimitiveMember {
+  pub fn member_name_for_index(index: usize) -> String {
+    format!("_{index}")
+  }
+
+  pub fn new<S, P>(name: S, r#type: P) -> Self
+  where
+    S: ToString,
+    P: Into<PrimitiveType>,
+  {
+    Self {
+      name: name.to_string(),
+      r#type: r#type.into(),
+    }
+  }
+}
+
+impl Display for PrimitiveMember {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}: {}", self.name, self.r#type)
+  }
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum PrimitiveComposition {
   Primitive(PrimitiveType),
   Composite(CompositeType),
-}
-
-impl PrimitiveComposition {
-  pub fn primitive_iter(&self) -> impl Iterator<Item = &PrimitiveType> {
-    match self {
-      PrimitiveComposition::Primitive(primitive) => {
-        Box::new(once(primitive)) as Box<dyn Iterator<Item = &PrimitiveType>>
-      }
-      PrimitiveComposition::Composite(composite) => Box::new(composite.primitive_iter()),
-    }
-  }
-
-  //TODO create memory layout
 }
 
 impl From<PrimitiveType> for PrimitiveComposition {
@@ -187,20 +269,164 @@ impl From<CompositeType> for PrimitiveComposition {
   }
 }
 
+impl PrimitiveComposition {
+  pub fn primitive_iter(&self) -> impl Iterator<Item = &PrimitiveType> {
+    match self {
+      PrimitiveComposition::Primitive(primitive) => {
+        Box::new(once(primitive)) as Box<dyn Iterator<Item = &PrimitiveType>>
+      }
+      PrimitiveComposition::Composite(composite) => Box::new(composite.primitive_iter()),
+    }
+  }
+
+  pub fn create_memory_layout(&self) -> MemoryLayout {
+    let mut primitive_members: Vec<_> = self
+      .primitive_iter()
+      .enumerate()
+      .map(|(index, primitive)| {
+        PrimitiveMember::new(
+          PrimitiveMember::member_name_for_index(index),
+          primitive.clone(),
+        )
+      })
+      .collect();
+    primitive_members.sort_by_cached_key(|member| -(member.r#type.alignment_power as isize));
+
+    let mut number_of_padding_bytes = 0;
+    if let Some(alignment_power) = primitive_members
+      .first()
+      .map(|member| member.r#type.alignment_power)
+      .filter(|&power| power > 0)
+    {
+      let alignment: usize = 1 << alignment_power;
+      let layout_size: usize = primitive_members
+        .iter()
+        .map(|member| member.r#type.size())
+        .sum();
+
+      let trailing_bytes = layout_size % alignment;
+      if trailing_bytes > 0 {
+        number_of_padding_bytes = alignment - trailing_bytes;
+      }
+    }
+
+    MemoryLayout {
+      primitive_members,
+      number_of_padding_bytes,
+    }
+  }
+}
+
+impl Display for PrimitiveComposition {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    match self {
+      PrimitiveComposition::Primitive(primitive) => Display::fmt(primitive, f),
+      PrimitiveComposition::Composite(composite) => Display::fmt(composite, f),
+    }
+  }
+}
+
+///Describes how a [`PrimitiveComposition`] will be lied out in memory
+pub struct MemoryLayout {
+  pub primitive_members: Vec<PrimitiveMember>,
+  pub number_of_padding_bytes: usize,
+}
+
+impl Display for MemoryLayout {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "[{}]",
+      self
+        .primitive_members
+        .iter()
+        .map(|member| member.to_string())
+        .chain(
+          Some(self.number_of_padding_bytes)
+            .iter()
+            .filter(|&&b| b > 0)
+            .map(|b| format!("+{b} padding bytes"))
+        )
+        .collect::<Vec<_>>()
+        .join(", ")
+    )
+  }
+}
+
 #[cfg(test)]
 mod test_primitive_type {
   use crate::environment::PrimitiveType;
 
   #[test]
   fn test_size() {
-    assert_eq!(4, PrimitiveType::new("f32", 4).size());
-    assert_eq!(16, PrimitiveType::new_aligned("vec4<f32>", 16, 16).unwrap().size());
-    assert_eq!(16, PrimitiveType {
-      name: "vec2<f64>".to_string(),
-      size_in_alignments: 2,
-      alignment_power: 3,
-    }.size());
+    assert_eq!(4, PrimitiveType::new("f32", 4, "f32").size());
+    assert_eq!(
+      16,
+      PrimitiveType::new_aligned("vec4<f32>", 16, 16, "glam::Vec4")
+        .unwrap()
+        .size()
+    );
+    assert_eq!(
+      16,
+      PrimitiveType {
+        name: "vec2<f64>".to_string(),
+        size_in_alignments: 2,
+        alignment_power: 3,
+        rust_equivalent: "glam::DVec2".to_string(),
+      }
+      .size()
+    );
   }
 }
 
-//TODO unit tests for CompositeType
+#[cfg(test)]
+mod test_composite_type {
+  use crate::environment::{CompositeType, Member, PrimitiveType};
+
+  #[test]
+  fn test_iter() {
+    let number_type = PrimitiveType::new("f32", 4, "f32");
+    let pixel_type = CompositeType::new("Pixel")
+      .with_member(Member::new("x", number_type.clone()))
+      .with_member(Member::new("y", number_type.clone()));
+    let str_type = PrimitiveType::new("str", 64, "String");
+    let named_pixel_type = CompositeType::new("NamedPixel")
+      .with_member(Member::new("name", str_type.clone()))
+      .with_member(Member::new("pixel", pixel_type));
+
+    let mut iter = named_pixel_type.primitive_iter();
+    assert_eq!(Some(&str_type), iter.next());
+    assert_eq!(Some(&number_type), iter.next());
+    assert_eq!(Some(&number_type), iter.next());
+    assert_eq!(None, iter.next());
+  }
+}
+
+#[cfg(test)]
+mod test_memory_layout_creation {
+  use crate::environment::{
+    CompositeType, Member, PrimitiveComposition, PrimitiveMember, PrimitiveType,
+  };
+
+  #[test]
+  fn test_create_memory_layout() {
+    let vec4_type = PrimitiveType::new_aligned("vec4<f32>", 16, 16, "glam::Vec4").unwrap();
+    let vec3_type = PrimitiveType::new("vec3<f32>", 12, "glam::Vec3");
+
+    let composition = PrimitiveComposition::from(
+      CompositeType::new("Vertex")
+        .with_member(Member::new("position", vec3_type.clone()))
+        .with_member(Member::new("color", vec4_type.clone())),
+    );
+    let layout = composition.create_memory_layout();
+    assert_eq!(
+      PrimitiveMember::new("_1", vec4_type.clone()),
+      layout.primitive_members[0]
+    );
+    assert_eq!(
+      PrimitiveMember::new("_0", vec3_type.clone()),
+      layout.primitive_members[1]
+    );
+    assert_eq!(4, layout.number_of_padding_bytes);
+  }
+}
