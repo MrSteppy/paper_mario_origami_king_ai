1d20abaf9ae859e4f721d1203a3bca23d5d5b00e
Steppy <jonlaurem@gmail.com>
2024-07-30 20:44:45 +0200

    Add PreProcessingEnvironment

--- shader_pre_processor/src/environment.rs
+++ shader_pre_processor/src/environment.rs
@@ -1,8 +1,41 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::hash::Hash;
 use std::iter::once;
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub struct PreProcessingEnvironment {
+  primitive_and_native_types: HashMap<String, PrimitiveComposition>,
+}
+
+impl PreProcessingEnvironment {
+  pub fn new() -> Self {
+    Self::default()
+  }
+
+  ///A native type is a type which is native in wgsl but can not be translated by wgsl_to_wgpu, like mat4x4<f32>.
+  /// Every type added which is not a [`PrimitiveType`] will be considered native.  
+  pub fn add<T>(&mut self, r#type: T)
+  where
+    T: Into<PrimitiveComposition>,
+  {
+    let r#type = r#type.into();
+    self.primitive_and_native_types.insert(
+      match &r#type {
+        PrimitiveComposition::Primitive(primitive) => &primitive.name,
+        PrimitiveComposition::Composite(native) => &native.name,
+      }
+      .to_owned(),
+      r#type,
+    );
+  }
+  
+  pub fn types(&self) -> &HashMap<String, PrimitiveComposition> {
+    &self.primitive_and_native_types
+  }
+}
 
 ///Describes a type which can be directly converted from wgsl to wgpu, like `f32` or `vec4<f32>`.
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
